5f6b2cdb5598071a4a763f0068f22b76de12151d
Ilya Dorogan <enotshannel@gmail.com>
2020-08-14 22:01:03 +0300

    lesoon_40_end

--- js/script.js
+++ js/script.js
@@ -128,3 +128,37 @@ function myAnim() {
 
 btn.addEventListener('click', myAnim);
 */
+
+/* LESSON 40 Работа с датами */
+
+// const now = new Date(2020, 7, 14, 20);
+/*
+const now = new Date();
+
+console.log(now.setHours(18)); // set установка значения
+console.log(now);
+*/
+
+/*
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getHours());
+console.log(now.getUTCHours()); //UTC
+
+console.log(now.getTimezoneOffset()); //разница от UTC в минутах
+console.log(now.getTime()); //прошедшее время в мс
+*/
+
+/*
+let start = new Date();
+
+for (let i = 0; i < 1000000; i++) {
+	let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Cycle end time ${end - start} ms`);
+*/
