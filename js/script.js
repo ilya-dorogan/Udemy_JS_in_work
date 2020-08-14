b37b528a325bae4852f238bd1bb9337eb2478566
Ilya Dorogan <enotshannel@gmail.com>
2020-08-14 18:49:58 +0300

    lesoon_39_end

--- js/script.js
+++ js/script.js
@@ -52,3 +52,79 @@ btn.classList.add('red');
 btn.classList.add('button');
 wrapper.append(btn);
 */
+
+/* LESSON 39 Скрипты и время их выполнения. setTimeout и setInterval */
+
+/*
+const timerId1 = setTimeout(function () {
+	console.log('1!');
+}, 1000);
+
+const timerI2 = setTimeout(function () {
+	console.log('2!');
+}, 2000);
+
+const timerId3 = setTimeout(function () {
+	console.log('3!');
+}, 3000);
+*/
+
+/*
+const timerID = setTimeout(function (text) {
+	console.log(text);
+}, 2000, 'Show this text');
+*/
+
+/*
+const btn = document.querySelector('.button');
+let timerID,
+	i = 0;
+
+function logger() {
+	if (i === 3) {
+		clearInterval(timerID);
+	}
+	console.log('text');
+	i++;
+}
+
+btn.addEventListener('click', () => {
+	// const timerID = setTimeout(logger, 2000);
+	timerID = setInterval(logger, 2000);
+
+});
+*/
+
+// const timerID = setTimeout(logger, 2000, 'Show this text');
+
+/*
+let id = setTimeout(function log() {
+	console.log('Work');
+	id = setTimeout(log, 500);
+}, 500); //рекурсивынй вызов
+*/
+
+/*
+const btn = document.querySelector('.btn');
+let timerId,
+	i = 0;
+
+function myAnim() {
+	const elem = document.querySelector('.box');
+	let pos = 0;
+
+	const id = setInterval(frame, 10);
+
+	function frame() {
+		if (pos == 300) {
+			clearInterval();
+		} else {
+			pos++;
+			elem.style.top = pos + 'px';
+			elem.style.left = pos + 'px';
+		}
+	}
+}
+
+btn.addEventListener('click', myAnim);
+*/
